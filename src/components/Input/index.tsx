bd1d128ca016eda508d2137e015f50a771fd81f3
Lucas Augusto <lucasaugustscode@gmail.com>
2023-07-21 09:21:07 -0300

    chore: remove unnecessary comments

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,15 +4,12 @@ import styles from './styles.module.css'
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
-// const regexCheckForCharacters = '/^[a-zA-Z]*$/'
-
 export const Input: React.FC<InputProps> = ({ ...props }) => {
   return (
     <input
       className={styles.container}
       type="text"
       placeholder="Adicione uma nova tarefa"
-      // pattern={regexCheckForCharacters}
       {...props}
     />
   )
